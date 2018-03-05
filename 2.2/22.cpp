24b90becdeba3b111ccbacfc0f221485a179cdf7
Леонид Шестёра < >
2018-03-05 11:02:02 +1000

    Добавьте файлы проекта.

---
+++ 2.2/22.cpp
@@ -0,0 +1,42 @@
+// 22
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+#include "stdafx.h"
+#include <math.h>
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include <windows.h>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+
+
+
+    return 0;
+}
+
