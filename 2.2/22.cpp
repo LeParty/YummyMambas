07736c920225a5263e85ff0198130e8b2c41ddba
Леонид Шестёра <lion21vek@mail.ru>
2018-06-19 19:44:13 +1000

    Склепал из всего, что было. (нужна ещё одна функция быстрого возведения в степень, эта не вывозит, не настолько быстрая)

--- 2.2/22.cpp
+++ 2.2/22.cpp
@@ -11,7 +11,144 @@
 #include <Windows.h>
 using namespace std;
 
-vector<int> q, p;
+vector<int> q, p, n, Fn, e, m;
+vector<int> ed = { { 1 } }, nol = {0};
+
+vector<int> ubrnol(vector<int>a)
+{
+	int noli = 0;
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (a[i] == 0)
+		{
+			noli++;
+		}
+		if (a[i] != 0)
+		{
+			break;
+		}
+	}
+
+	a.erase(a.begin(), a.begin() + noli);
+	/*
+	if (a.size() == 0)
+	{
+		a.push_back(0);
+	}
+	*/
+	return a;
+}
+
+typedef std::string  T_num_s;
+///
+void  del_leading_zero(T_num_s&  a)
+{
+	while (a.size() > 1
+		&& a[0] == '0')
+	{
+		a.erase(0, 1);
+	}
+}
+///
+bool less_for_big_int(T_num_s  a, T_num_s  b)
+{
+	del_leading_zero(a);
+	del_leading_zero(b);
+
+	return  a.size() == b.size() ? a < b : a.size() < b.size();
+}
+///
+void  reduce_big_int(T_num_s&  minuend, const T_num_s&  subtrahend)
+{
+	for (T_num_s::size_type cur_pos = 0; cur_pos < subtrahend.size(); ++cur_pos)
+	{
+		T_num_s::size_type  minuend_cur_pos = minuend.size() - 1 - cur_pos;
+		T_num_s::size_type  subtrahend_cur_pos = subtrahend.size() - 1 - cur_pos;
+
+		char&        cur_minuend_dig_ref = minuend[minuend_cur_pos];
+		const char&  cur_subtrahend_dig_ref = subtrahend[subtrahend_cur_pos];
+
+		if (cur_minuend_dig_ref >= cur_subtrahend_dig_ref)
+		{
+			cur_minuend_dig_ref -= cur_subtrahend_dig_ref - '0';
+		}
+		else
+		{
+			(cur_minuend_dig_ref -= cur_subtrahend_dig_ref - '0') += 10;
+			for (int i = 1; ; ++i)
+			{
+				if (minuend[minuend_cur_pos - i] == '0')
+				{
+					minuend[minuend_cur_pos - i] = '9';
+				}
+				else
+				{
+					--minuend[minuend_cur_pos - i];
+					break;
+				}
+			}
+		}
+		del_leading_zero(minuend);
+	}
+	del_leading_zero(minuend);
+}
+///
+void  inc_big_int(T_num_s&  a)
+{
+	for (T_num_s::size_type cur_pos = a.size() - 1;; --cur_pos)
+	{
+		if (a[cur_pos] < '9')
+		{
+			++a[cur_pos];
+			return;
+		}
+		else
+		{
+			a[cur_pos] = '0';
+			if (cur_pos == 0)
+			{
+				a.insert(0, "1");
+				return;
+			}
+		}
+	}
+}
+///
+T_num_s  div_big_int(const T_num_s&  a, const T_num_s&  b)
+{
+	if (b == "0")
+	{
+		return  "division into zero";
+	}
+
+	T_num_s  res = "0";
+	T_num_s  minuend = a;
+	T_num_s  subtrahend = b;
+
+	while (subtrahend.size() < minuend.size())
+	{
+		subtrahend += '0';
+	}
+
+	for (;;)
+	{
+		while (!less_for_big_int(minuend, subtrahend))
+		{
+			reduce_big_int(minuend, subtrahend);
+			inc_big_int(res);
+		}
+		if (subtrahend.size() <= b.size())
+		{
+			break;
+		}
+
+		subtrahend.erase(subtrahend.size() - 1);
+		res += '0';
+		del_leading_zero(res);
+	}
+
+	return  res;
+}
 
 vector<int> create(vector<int> mas)
 {
@@ -33,6 +170,32 @@ vector<int> create(vector<int> mas)
 		}
 	}
 	return mas;
+}
+
+void ProvVec()
+{
+	if (q.size() > p.size())
+	{
+		return;
+	}
+	if (q.size() < p.size())
+	{
+		vector<int> q_v = q;
+		q = p;
+		p = q_v;
+		return;
+	}
+	if (q.size() == p.size())
+	{
+		if (q < p)
+		{
+			vector<int> q_v = q;
+			q = p;
+			p = q_v;
+		}
+		return;
+	}
+	return;
 }
 
 void ShowVector(vector<int> mas)
@@ -75,6 +238,21 @@ vector<int> Umn(vector<int> mas1, vector<int> mas2)
 	vector<int> res;
 	vector<vector<int>> mul;
 
+	if (mas1.size() == 1 && mas2.size() == 1)
+	{
+		int g = mas1[0] * mas2[0];
+		if ((g / 10) > 0)
+		{
+			int n = g;
+			g = g / 10;
+			res.push_back(g);
+			g = n;
+			g = g % 10;
+			res.push_back(g);
+		}
+		else
+		res.push_back(g);
+	}
 	for (int i = mas1.size() - 1; i >= 0; i--)
 	{
 		vector<int> tmp;
@@ -113,11 +291,10 @@ vector<int> Umn(vector<int> mas1, vector<int> mas2)
 		mul[0][i] = (tmp + c) % 10;
 		c = (tmp + c) / 10;
 	}
-	//mul[0][mul[0].size() - 1] += c;
 
 	int c_copy = c;
 
-	while (c_copy > 1)
+	while (c_copy >= 1)
 	{
 		int a = c_copy % 10;
 		mul[0].push_back(a);
@@ -129,137 +306,683 @@ vector<int> Umn(vector<int> mas1, vector<int> mas2)
 		res.push_back(mul[0][mul[0].size() - 1 - i]);
 	}
 
-	return res;
-	//return mul[0];
-}
-
-vector<int> vich(vector<int> mas1, vector<int> mas2)
-{
-	/*
-	int h = mas1.size() - mas2.size();
-	if (h >= 3)
+	int noli = 0;
+	for (int i = 0; i < res.size(); i++)
 	{
-	vector<int> mas2tmp = mas2;
-	for (int i = 0; i < h - 1; i++)
-	{
-	mas2tmp.push_back(0);
-	//mas2 = mas2 * 10;
-	}
-	mas1 = vich(mas1, mas2tmp);
-	//return vich(mas1, mas2);
-	}
-	*/
-	if (mas1.size() < mas2.size())
-	{
-		mas1.insert(mas1.begin(), mas2.size() - mas1.size(), 0);
-		return vich(mas2, mas1);
-	}
-	if (mas2.size() < mas1.size())
-	{
-		mas2.insert(mas2.begin(), mas1.size() - mas2.size(), 0);
-		//return vich(mas2, mas1);
-	}
-	if (mas1.size() == mas2.size())
-	{
-		if (mas1[0] < mas2[0])
+		if (res[i] == 0)
 		{
-			return vich(mas2, mas1);
+			noli++;
 		}
-		else
+		if (res[i] != 0)
 		{
-			vector<int> res;
-			int c = 0;
-
-			for (int j = mas1.size() - 1; j >= 0; j--)
-			{
-
-				if ((mas1[j] - c) >= mas2[j])
-				{
-					int a = mas1[j] - c - mas2[j];
-					res.push_back(a);
-					c = 0;
-				}
-				if ((mas1[j] - c) < mas2[j])
-				{
-					int a = mas1[j] + 10 - c - mas2[j];
-					c = 1;
-					res.push_back(a);
-				}
-			}
-			vector<int> res1;
-			for (int i = 0; i < res.size(); i++)
-			{
-				res1.push_back(res[res.size() - 1 - i]);
-			}
-			return res1;
-		}
-	}
-}
-
-vector<int> Evklid(vector<int> mas1, vector<int> mas2)
-{
-	bool Ch = 0;
-	for (int i = 0; i < mas2.size(); i++)
-	{
-		if (mas2[i] != 0)
-		{
-			Ch = 1;
 			break;
 		}
 	}
-	if (Ch == 0)
+
+	res.erase(res.begin(), res.begin() + noli);
+
+
+	return res;
+}
+
+
+
+vector<int> minusq(vector<int> a, vector<int> b)
+{
+	vector<int> res;
+	res.resize(a.size());
+	if (a.size() > b.size())
 	{
-		return mas1;
-	}
-	if (Ch == 1)
-	{
-		Ch = 0;
-		//vector<int> tmp = mas1;
-		while (mas1 >= mas2)
+		while (a.size() > b.size())
 		{
-			mas1 = vich(mas1, mas2);
+			b.insert(b.begin(), 0);
 		}
-		return Evklid(mas2, mas1);
+	}
+	int c = 0;
+	for (int i = a.size() - 1; i >= 0; i--)
+	{
+		if (a[i] >= (b[i] + c))
+		{
+			res[i] = a[i] - b[i] - c;
+			c = 0;
+		}
+		else
+		{
+			if (a[i] < (b[i] + c))
+			{
+				res[i] = (a[i] + 10) - (b[i] + c);
+				c = 1;
+			}
+		}
+	}
+
+	//res = ubrnol(res);
+
+	return res;
+}
+
+
+
+bool GG = 0, Gg = 0;
+
+vector<int> vich(vector<int> a, vector<int> b) // Деление с остатком (выдаёт остаток от деления)
+{
+	vector<int> b_copy_s = b;
+
+	while (b_copy_s.size() < a.size())
+	{
+		b_copy_s.insert(b_copy_s.begin(), 0);
+	}
+
+	while (a >= b_copy_s)
+	{
+		a = ubrnol(a);
+
+		if (a.size() == b.size())
+		{
+			while (a > b)
+			{
+				a = minusq(a, b);
+			}
+			if (a == b)
+			{
+				a = minusq(a, b);
+			}
+			a = ubrnol(a);
+			return a;
+		}
+		else
+		{
+			vector<int> b_copy = b;
+
+			while (b_copy.size() < a.size())
+			{
+				b_copy.push_back(0);
+			}
+
+			if (a > b_copy)
+			{
+				a = minusq(a, b_copy);
+			}
+			else
+			{
+				b_copy = b;
+				int uravn = a.size() - b.size();
+
+				vector<int> del = {};
+				vector<int> na = {};
+
+				na.resize(b.size() + 1);
+
+				for (int i = 0; i < b.size() + 1; i++)
+				{
+					na[i] = a[i];
+				}
+
+				b_copy.insert(b_copy.begin(), 0);
+
+				int dil = 0;
+
+				while (na > b_copy)
+				{
+					na = minusq(na, b_copy);
+					dil++;
+				}
+
+				del.push_back(dil);
+
+				if (b_copy.size() > del.size())
+				{
+					del.insert(del.begin(), 0);
+				}
+
+				b_copy = Umn(b_copy, del);
+
+				while (b_copy.size() < a.size())
+				{
+					b_copy.push_back(0);
+				}
+
+				if (b_copy > a)
+				{
+					b_copy.erase(b_copy.begin() + b_copy.size() - 1, b_copy.begin() + b_copy.size());
+				}
+
+				a = minusq(a, b_copy);
+
+				while (b_copy_s.size() > a.size())
+				{
+					a.insert(a.begin(), 0);
+				}
+			}
+		}
+	}
+	a = ubrnol(a);
+	return a;
+}
+
+
+vector<int> Prostoe(vector<int> a, vector<int> b)
+{
+	a = vich(a, b);
+	if (a.size() == 0)
+	{
+		return b;
+	}
+	else
+	{
+		return Prostoe(b, a);
 	}
 }
 
-/*
-vector<int> Prostoe(vector<int> mas)
+vector<int> F_n(vector<int> a, vector<int> b)
 {
-int G = mas.size() / 2 + 1;
-int g = 0;
-for (int i = 0; i < G; i++)
-{
-
+	a = minusq(a, ed);
+	b = minusq(b, ed);
+	vector<int> res = Umn(a, b);
+	return res;
 }
 
+vector<int> ecreate(vector<int> a)
+{
+	vector<int> res;
+
+	for (int i = 1; i < 2; i++)
+	{
+		int hz = rand() % 8 + 2;
+		res.push_back(hz);
+
+		//ShowVector(res);
+		vector<int> tmp;
+
+		tmp = Prostoe(a, res);
+		tmp = minusq(tmp, ed);
+		tmp = ubrnol(tmp);
+
+		if (tmp.size() == 0)
+		{
+			return res;
+		}
+		else
+		{
+			res = minusq(res, res);
+			res = ubrnol(res);
+
+			//int b; // пауза для проверки
+			//cin >> b;
+			i--;
+		}
+	}
 }
-*/
+
+vector<int> plusq(vector<int> a, vector<int> b)
+{
+	vector<int> res;
+	res.resize(a.size());
+	int min_dl = a.size();
+	if (a.size() < b.size())
+	{
+		min_dl = a.size();
+		res.resize(b.size());
+		res = b;
+	}
+	if (a.size() > b.size())
+	{
+		min_dl = b.size();
+		res.resize(a.size());
+		res = a;
+	}
+
+	for (int i = 0; i < min_dl; i++)
+	{
+		res[res.size() - 1 - i] = a[a.size() - 1 - i] + b[b.size() - 1 - i];
+	}
+
+	int c = 0;
+
+	for (int i = 0; i < res.size(); i++)
+	{
+		int tmp = res[res.size()- 1 - i];
+		res[res.size() - 1 - i] = (tmp + c) % 10;
+		c = (tmp + c) / 10;
+	}
+
+	int c_copy = c;
+
+	while (c_copy >= 1)
+	{
+		int a = c_copy % 10;
+		//res.push_back(a);
+		res.insert(res.begin(), a);
+		c_copy = c_copy / 10;
+	}
+	return res;
+}
+
+vector<int> delenie(vector<int> a, vector<int> b)
+{
+	vector<int> b_copy_s = b, res = {1};
+	vector<vector<int>> resultat;
+
+	while (b_copy_s.size() < a.size())
+	{
+		b_copy_s.insert(b_copy_s.begin(), 0);
+	}
+	int o = 0;
+	if (a < b_copy_s)
+	{
+		return nol;
+	}
+	while (a >= b_copy_s)
+	{
+		a = ubrnol(a);
+		res = minusq(res, res);
+		res = ubrnol(res);
+		if (a.size() == b.size())
+		{
+			int ch = 0;
+			while (a >= b)
+			{
+				a = minusq(a, b);
+				ch++;
+			}
+			res.push_back(ch);
+			if (resultat.size() < (o + 1))
+			{
+				resultat.resize(o + 1);
+			}
+			resultat[o] = res;
+			while (b_copy_s.size() > a.size())
+			{
+				a.insert(a.begin(), 0);
+			}
+			//a = ubrnol(a);
+			//return a;
+		}
+		else
+		{
+			vector<int> b_copy = b;
+			int ch_2 = 0;
+			while (b_copy.size() < a.size())
+			{
+				b_copy.push_back(0);
+				ch_2++;
+			}
+
+			if (a >= b_copy)
+			{
+				int ch = 0;
+				while(a >= b_copy)
+				{ 
+				a = minusq(a, b_copy);
+				ch++;
+				}
+				res.push_back(ch);
+				if (resultat.size() < (o + 1))
+				{
+					resultat.resize(o + 1);
+				}
+				resultat[o] = res;
+				for (int i = 0; i < ch_2; i++)
+				{
+					resultat[o].push_back(0);
+				}
+			}
+			else
+			{
+				b_copy = b;
+				
+				vector<int> del = {};
+				vector<int> na = {};
+
+				na.resize(b.size() + 1);
+
+				for (int i = 0; i < b.size() + 1; i++)
+				{
+					na[i] = a[i];
+				}
+
+				b_copy.insert(b_copy.begin(), 0);
+
+				int dil = 0;
+
+				while (na >= b_copy)
+				{
+					na = minusq(na, b_copy);
+					dil++;
+				}
+				if (dil < 10)
+				{
+					del.push_back(dil);
+
+					res.push_back(dil);
+				}
+				if (dil == 10)
+				{
+					del.push_back(1);
+					del.push_back(0);
+					res.push_back(1);
+					res.push_back(0);
+				}
+
+				dil = 0;
+				b_copy = Umn(b_copy, del);
+
+				while (b_copy.size() < a.size())
+				{
+					b_copy.push_back(0);
+					dil++;
+				}
+
+				if (b_copy > a)
+				{
+					b_copy.erase(b_copy.begin() + b_copy.size() - 1, b_copy.begin() + b_copy.size());
+					dil--;
+				}
+
+				for (int i = 0; i < dil; i++)
+				{
+					res.push_back(0);
+				}
+
+				a = minusq(a, b_copy);
+
+				while (b_copy_s.size() > a.size())
+				{
+					a.insert(a.begin(), 0);
+				}
+				if (resultat.size() < (o + 1))
+				{
+					resultat.resize(o + 1);
+				}
+				resultat[o] = res;
+			}
+
+		}
+		o++;
+	}
+	//a = ubrnol(a);
+	for (int i = 1; i < resultat.size(); i++)
+	{
+		resultat[0] = plusq(resultat[0], resultat[i]);
+	}
+	return resultat[0];
+}
+
+vector<int>DVA(vector<int> q1) // переводит число в двоичную систему
+{
+	//func();
+	vector<int> q_dva;
+
+	vector<int> d, dv = { { 2 } };
+	int d2;
+
+	for (;;)
+	{
+		d = vich(q1, dv);
+		d = ubrnol(d);
+
+		if (d.size() == 0)
+		{
+			d.push_back(0);
+			q_dva.push_back(0);
+		}
+		else
+		{
+			q_dva.push_back(1);
+		}
+
+		q1 = delenie(q1, dv);
+		q1 = ubrnol(q1);
+		if (q1.size() == 0)
+		{
+			break;
+		}
+	}
+
+	vector<int> res;
+	res.resize(q_dva.size());
+	for (int i = 0; i < q_dva.size(); i++)
+	{
+		res[i] = q_dva[q_dva.size() - 1 - i];
+	}
+	return res;
+}
+
+vector<int> Stepen(vector<int> a, vector<int> s)
+{
+	int k;
+	vector<vector<int>> mas;
+	mas.resize(s.size());
+	//vector<int> ed = { 1 };
+	for (int i = 0; i < s.size(); i++)
+	{
+		k = s[i];
+		if (i == s.size() - 1)
+		{
+			if (k == 0)
+			{
+				mas[i].push_back(1);
+				i++;
+				break;
+			}
+			mas[i] = a;
+			i++;
+			break;
+		}
+		if (k == 1)
+		{
+			mas[i] = a;
+		}
+		if (k == 0)
+		{
+			mas[i].push_back(1);
+		}
+	}
+	for (int i = 1; i < mas.size(); i++)
+	{
+		if (i == mas.size() - 1)
+		{
+			mas[0] = Umn(mas[0], mas[i]);
+			i++;
+			break;
+		}
+		if (i == 1)
+		{
+			mas[0] = Umn(mas[0], mas[0]);
+		}
+		//mas[0] = mas[0] * mas[i];
+		mas[0] = Umn(mas[0], mas[i]);
+
+		mas[0] = Umn(mas[0], mas[0]);
+	}
+
+	return mas[0];
+}
+
+
+
 
 int main()
 {
 	srand(time(0));
-	//func();
-	//vector<int>resUm = Umn(q, p);
-	//ShowVector(resUm);
+	//func(); // Ввод чисел с клавиатуры
 
-	q = create(q);
+	//create - создание чисел заданной длины
+	{
+		/*
+		q = create(q);
+		//ShowVector(q);
+		p = create(p);
+		//ShowVector(p);
+		ProvVec();
+		*/
+	}
+
+	//show - вывод чисел q и p
+	{
+		/*
+	cout << "q = ";
 	ShowVector(q);
 
-	p = create(p);
+	cout << "p = ";
 	ShowVector(p);
+	*/
+	}
 
-	//vector<int>resUm = Umn(q, p);
+	//Евклид, поиск общего делителя
+	{
+	/*
+	vector<int>resEvklid = Prostoe(q, p);
+	cout << "Obshii delitel' = ";
+	ShowVector(resEvklid);
+	*/
+	}
+
+	//Вычитание
+	{
+	//vector<int>resUm = minusq(q, p);
+	//resUm = ubrnol(resUm);
 	//ShowVector(resUm);
+	}
 
-	//vector<int>resVi = vich(q, p);
-	//ShowVector(resVi);
+	//Деление с остатком
+	{
+		//vector<int>resVi = vich(q, p);
+		//ShowVector(resVi);
+	}
 
-	vector<int>resEv = Evklid(q, p);
-	cout << "obshii delitel = ";
-	ShowVector(resEv);
+	//Деление без остатка(Илья\нужно подогнать под вектора)
+	/*
+	for (;;)
+	{
+		std::cout << "a = ";
+		T_num_s  a;
+		std::cin >> a;
+		del_leading_zero(a);
+
+		std::cout << "b = ";
+		T_num_s  b;
+		std::cin >> b;
+		del_leading_zero(b);
+
+		std::cout << "a / b = "
+			<< div_big_int(a, b)
+			<< std::endl
+			<< std::endl
+			<< std::endl;
+	}
+
+	*/
+
+	//Умножение
+	{
+	/*
+	vector<int>resUm = Umn(q, p);
+	ShowVector(resUm);
+	*/
+	}
+
+	//Сложение
+	/*
+	vector<int> sum = plusq(q, p);
+	ShowVector(sum);
+	*/
+
+	//Деление
+	
+	{
+	/*
+	vector<int> delen = delenie(q, p);
+	ShowVector(delen);
+	*/
+	}
+	
+
+	//Перепод числа в двоичный код
+	{
+		/*
+		e = { 104 };
+		vector<int> e_dv = DVA(e);
+		ShowVector(e_dv);
+		*/
+	}
+
+	//Довольно быстрое возведение в степень )) 
+	{
+		
+		//m = create(m);
+		m = { {6}, {7} };
+		ShowVector(m);
+		//func();
+		//ShowVector(q);
+		cout << endl;
+
+		e = { {1},{3},{0},{1} }; // изначальная степень(в десятиричной системе)
+		ShowVector(e);
+		cout << endl;
+
+		vector<int> e_dv = DVA(e); // Та же степень, но уже в двоичной системе
+		ShowVector(e_dv);
+		cout << endl;
+		vector<int> shif = Stepen(m, e_dv);//возводим число в нужную степень
+
+		ShowVector(shif);
+		cout << endl;
+		
+	}
 
 
+
+	//RSA алг.
+	/*
+	{
+		//create - создание чисел заданной длины
+		{
+			q = create(q);
+			//ShowVector(q);
+			p = create(p);
+			//ShowVector(p);
+			ProvVec();
+			//Проверка на простоту!!!!!!!!!!!
+		}
+		//show - вывод чисел q и p
+		{
+			cout << "q = ";
+			ShowVector(q);
+			cout << endl;
+
+			cout << "p = ";
+			ShowVector(p);
+			cout << endl;
+		}
+		//Вычисляем n - модуль.
+		{
+			n = Umn(q, p);
+			cout << "n = ";
+			ShowVector(n);
+			cout << endl;
+		}
+		//Вычисляем F(n) = (q - 1)(p - 1);
+		{
+			Fn = F_n(q, p);
+			cout << "Fn = ";
+			ShowVector(Fn);
+			cout << endl;
+		}
+		//Подбираем e (1 < e < Fn), взаимнопростую с Fn; 
+		{
+			e = ecreate(Fn);
+			cout << "e = ";
+			ShowVector(e);
+			cout << endl;
+		}
+		
+
+
+	
+	}
+	*/
 
 
 
